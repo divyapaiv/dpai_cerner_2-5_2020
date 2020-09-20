d9b12d2c9efb9aae97b8e571572f07d7731b4aeb
divyapaiv <divyapai993@gmail.com>
2020-09-20 10:19:19 +0530

    DAY-6 C

---
+++ C/quoandrem.c
@@ -0,0 +1,32 @@
+// cerner_2^5_2020
+// Run at any online c editor
+
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+int main(void) {
+         int dividend=21,divisor=10,quotient = 0;
+	//div bdivisor 0
+	if (divisor == 0)
+	{
+		printf("Error!! Divisible bdivisor 0");
+		exit(1);
+	}
+
+	//Get the sign of the results  by multiplying the number
+	int sign = 1;
+	if (dividend * divisor < 0)
+		sign = -1;
+
+	// convert both dividend and divisor to positive
+	dividend = abs(dividend), divisor = abs(divisor);
+	// loop till dividend dividend is more than the divisor divisor
+	while (dividend >= divisor)
+	{dividend = dividend - divisor;
+	quotient++;
+	}
+	printf("Remainder is %d\n", dividend);
+	printf("Quotient is %d\n",  sign * quotient);
+
+}
