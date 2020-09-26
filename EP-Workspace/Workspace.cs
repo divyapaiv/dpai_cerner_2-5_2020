546343483ca401abf125fbbbd24340b803d5344e
divyapaiv <divyapai993@gmail.com>
2020-09-26 21:00:45 +0530

    day 13 c#

--- EP-Workspace/Workspace.cs
+++ EP-Workspace/Workspace.cs
@@ -8,14 +8,14 @@ class WorkSpace {
        Console.WriteLine("*******Let's clean the workspace in free time\n");
        Console.WriteLine("*******Remove distractions\n");
        Console.WriteLine("*******Remove crumpled papers\n");
-       Console.WriteLine("*******Remove Items which are no longer will be used!\n");
+       Console.WriteLine("*******Remove Items which are no longer in use!\n");
        return true;
   }
   void findadocument(string doc)
   { 
       Console.WriteLine("#######Finding the document");
       if(this.isTidy==true)
-          Console.WriteLine(" "+doc+" within a few minutes! YEYY!\n");
+          Console.WriteLine(" "+doc+" found within a few minutes! YEYY!\n");
       else
           Console.WriteLine("There is a deadline I can not find my document as my workspace is filled with unneccessary items\n and the meeting will start in few minutes");
   }
